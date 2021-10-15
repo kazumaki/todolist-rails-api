3a199c199a9840235c07b8d867c378647748e020
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-10-15 17:26:58 -0300

    add length and format validation for email model and it respective tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { minimum: 5 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
   validates :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:user) { create(:user) }
+
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:email).is_at_least(5) }
   it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:password_digest) }
   it { should have_secure_password }
+
+  it 'Do not allow emails with bad format' do
+    user.email = 'foo@bar'
+    expect(user).to be_invalid
+  end
 end
