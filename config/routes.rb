21e6775c32147b12a9f459c6af051e6acb819b86
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-10-15 17:36:32 -0300

    add users and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'sessions/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :sessions, only: %i[create]
+      resources :users, only: %i[create]
+    end
+  end
 end
