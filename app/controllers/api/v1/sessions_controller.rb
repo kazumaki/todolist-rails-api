012364bc31ff4efa8a43425557375f10b91dedd7
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-10-17 11:41:59 -0300

    add jwt gem; add tests for sessions controller; add sessions create method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: user_params[:email])
+
+    unless @user
+      head :unprocessable_entity
+      return
+    end
+
+    if @user.authenticate(user_params[:password])
+      secret_key = Rails.application.secrets.secret_key_base.to_s
+      token = JWT.encode({user_id: @user.id}, secret_key)
+      render json: { user: @user, token: token }, status: :created
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Sessions", type: :request do
-  let(:valid_user) { User.create(email: 'foo@bar.foo', password: '123456') }
+  let(:valid_user) { User.create(email: 'foo@bar.foo', password: '123456', password_confirmation: '123456') }
 
   describe "POST /sessions" do
     context 'when the user data is valid' do
       before { post '/api/v1/sessions', params: { user: { email: valid_user.email, password: valid_user.password } } }
+
+      it { expect(JSON.parse(response.body)).to have_key('token') }
+      it { expect(JSON.parse(response.body)).to have_key('user') }
+      it { expect(response).to have_http_status(:created) }
+    end
+
+    context 'when the password is invalid' do
+      before { post '/api/v1/sessions', params: { user: { email: valid_user.email, password: '1234' } } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+
+    context 'when the email is invalid' do
+      before { post '/api/v1/sessions', params: { user: { email: 'foo@bar', password: '123456' } } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
     end
   end
 end
