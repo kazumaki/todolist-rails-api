7c4c013e24e83254a45f9bcdcee0dde675b0e2e0
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-10-18 14:57:09 -0300

    add authenticate method for todo index method; add tests for todos controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::TodosController < ApplicationController
 
+  before_action :authenticate, only: %i[index]
+
   def index
-    @user = User.first
+    render json: @current_user.todos, status: :ok
+  end
 
-    unless @user
-      head :unauthorized
-      return
+  private
+  def authenticate
+    token = request.headers['Authorization']
+    secret_key = Rails.application.secrets.secret_key_base.to_s
+
+    begin
+      user_id = HashWithIndifferentAccess.new(JWT.decode(token, secret_key)[0])[:user_id]
+      @current_user = User.find(user_id)
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
     end
-
-    render json: @user.todos, status: :ok
   end
 
 end
--- spec/requests/api/v1/todos_spec.rb
+++ spec/requests/api/v1/todos_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe "Api::V1::Todos", type: :request do
     let!(:user) { create(:user) }
     let!(:todo_list) { create_list(:todo, 5, user: user) }
     context 'when the user is logged in' do
-      before { get '/api/v1/todos' }
+      before { get '/api/v1/todos', headers: { 'Authorization': JWT.encode({user_id: user.id}, Rails.application.secrets.secret_key_base.to_s)}}
+      it { expect(JSON.parse(response.body)).to have_attributes(length: 5) }
       it { expect(response).to have_http_status(:ok) }
+    end
+
+    context 'when the header token is invalid' do
+      before { get '/api/v1/todos' }
+      it { expect(response).to have_http_status(:unauthorized) }
     end
   end
 end
